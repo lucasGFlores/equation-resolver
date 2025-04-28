eec8bf3f696c9dcfbc5126fa447fbfc27bf909fc
lucasGFlores <lucasgabrielflores25@gmail>
2025-04-28 16:15:57 -0300

    refact: add matrix e resolver to split the responsability

---
+++ src/model/matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+class Matrix:
+    """
+    responsibilities:
+        - calculate determinant
+        - change columns
+    """
+    def __init__(self, matrix: np.array ):
+        pass
+
+    def determinant(self) -> float:
+        pass
+
+    def change_column(self,column: int) -> None:
+        pass
